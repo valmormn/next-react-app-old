c78813596f543325adee7581c13059fe1b4bda98
Valmor <valmormn@gmail.com>
2021-09-21 17:00:20 -0300

    config husky & lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-//const foo = "buzz";
+//const foo = 'buzz'
 
 const Home: NextPage = () => {
   return (
